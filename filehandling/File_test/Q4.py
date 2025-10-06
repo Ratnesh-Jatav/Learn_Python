419dcb712f704b2781591888d72b9ce2a4313032
Ratnesh Jatav <ratneshjatav909@gmail.com>
2025-10-06 23:54:19 +0530

    File I/O2

--- filehandling/File_test/Q4.py
+++ filehandling/File_test/Q4.py
@@ -1,2 +1,12 @@
 # A file contains a word "Donkey" multiple time you need to replace it with "#######" by updating the same file.
 
+words = ["Donkey", "bad", "ganda"]
+
+with open("Q4.txt", "r") as file:
+    content = file.read()
+
+    for word in words:
+        content = content.replace(word, "#" * len(word))
+    
+with open("Q4.txt", "w") as file:
+    file.write(content)
